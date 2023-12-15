b5037f3375eacfc9c773b8efd29c86a6efcd0463
BT_1N3_09 <bths@edu.com>
2023-12-15 09:23:09 -0500

    updated print

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args){
         int i = 991;
-        System.out.println("i changed itasd");
+        System.out.println("hwy did le chicken cross le road?");
         System.out.println("hello");
         System.out.println("averagekidinhohio");
         System.out.println("DWAJDWLAJDIWLAJDWIOALJDDDDDDDDWAL");
