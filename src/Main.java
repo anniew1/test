26e10e099182fbf83171b0c0c7dfae3527546bc5
BT_1N3_08 <dwa@bths.edu>
2023-12-15 09:20:50 -0500

    switched print

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args){
         int i = 991;
-        System.out.println("Jasmine printed");
+        System.out.println("djwaildjwail");
         System.out.println("hello");
         System.out.println("averagekidinhohio");
         System.out.println("DWAJDWLAJDIWLAJDWIOALJDDDDDDDDWAL");
